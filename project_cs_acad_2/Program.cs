51348f62702c4250d85891b0fc41810efa232c30
Dhruv Shah <dushah2002@gmail.com>
2023-05-27 23:54:55 +0530

    some reformtting

--- project_cs_acad_2/Program.cs
+++ project_cs_acad_2/Program.cs
@@ -1,160 +1,180 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Data.SqlClient;
 using System.Data.SQLite;
+using System.Linq;
 
 namespace project_cs_acad_2
 {
     internal class Program
     {
-
-        static public (string,int) Get_vals()
-        {
-            string input_date;
-            int input_glasses;
-            Console.WriteLine("Enter the date(dd-mm-yy): ");
-            input_date = Console.ReadLine();
-            Console.WriteLine("Enter the amount of glasses: ");
-            input_glasses = Convert.ToInt32(Console.ReadLine());
-
-            return (input_date,input_glasses);  
-        }
+        private static string databaseFile = $@"{AppDomain.CurrentDomain.BaseDirectory}habit-tracker.db";
+        private static string connectionString = $"Data Source={databaseFile}";
 
         static void Main(string[] args)
         {
-
-
-            // Connection string
-            string databaseFile = $@"{AppDomain.CurrentDomain.BaseDirectory}habit - tracker.db";
-
             if (!System.IO.File.Exists(databaseFile))
             {
-                // Create a new SQLite database file
                 SQLiteConnection.CreateFile(databaseFile);
+                InitializeDatabase();
             }
 
-            string connectionString = $"Data Source={databaseFile}";
-
-            // Create a new connection
-            SQLiteConnection connection = new SQLiteConnection(connectionString);
-            //Console.WriteLine(connectionString);
-
-
-            // Open the connection
-            connection.Open();
-
-            var tablecmd = connection.CreateCommand();
-
-            tablecmd.CommandText = @"CREATE TABLE IF NOT EXISTS water_habit(
-
-                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                    Date TEXT,
-                                    Glasses INTEGER)";
-
-
-            tablecmd.ExecuteNonQuery();
-            bool App_on = true;
-            bool menu_show = true;
+            bool appOn = true;
+            bool menuShow = true;
             string userInput = "";
 
-            while (App_on)
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
             {
-                if (menu_show)
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                while (appOn)
                 {
-                    // Perform database operations here
-                    Console.WriteLine("what would you like to do?\n");
-                    Console.WriteLine("V - view logs");
-                    Console.WriteLine("I - insert an entry");
-                    Console.WriteLine("U - update an entry");
-                    Console.WriteLine("D - delete an entry");
-                    Console.WriteLine("Q - quit the app");
+                    if (menuShow)
+                    {
+                        ShowMenu();
+                        userInput = Console.ReadLine();
+                    }
 
-                    userInput = Console.ReadLine();
+                    while (!IsValidOperation(userInput))
+                    {
+                        Console.WriteLine("Enter a valid operation");
+                        userInput = Console.ReadLine();
+                    }
 
-                }
-                while (!(new string[] { "V", "I", "U", "D" , "Q" }.Contains(userInput)))
-                {
-                    Console.WriteLine("Enter valid operation");
-                    userInput = Console.ReadLine();
-                }
+                    Console.Clear();
+                    menuShow = false;
 
-                Console.Clear();
-                menu_show = false;  
-
-                switch (userInput.Trim().ToUpper())
-                {
-
-                    case "V":
-
-                        tablecmd.CommandText = "SELECT Date,Glasses FROM water_habit";
-                        SQLiteDataReader reader = tablecmd.ExecuteReader();
-
-                        Console.WriteLine("Date\t\tGlasses");
-                        Console.WriteLine("-------------------------");
-
-                        while (reader.Read())
-                        {
-                            Console.WriteLine($"{reader["Date"]}\t\t {reader["Glasses"]}");
-                        }
-
-                        reader.Close();
-
-                        Console.WriteLine("Press any key to continue");
-                        Console.ReadKey();
-                        menu_show= true;
-                        Console.Clear();
-
-                        break;
-
-                    case "I":
-
-                        Console.WriteLine("Enter data to be Inserted: ");
-                        var result_insert = Get_vals();
-                        tablecmd.CommandText = $"INSERT INTO water_habit(Date,Glasses) VALUES('{result_insert.Item1}',{result_insert.Item2})";
-
-                        tablecmd.ExecuteNonQuery();
-                        menu_show = true;
-                        Console.Clear();
-
-                        break;
-
-                    case "U":
-
-                        Console.WriteLine("Enter data to be updated");
-                        var result_update = Get_vals();
-
-                        tablecmd.CommandText = $"UPDATE water_habit SET glasses = {result_update.Item2} WHERE date = {result_update.Item1}";
-                        tablecmd.ExecuteNonQuery();
-                        menu_show = true;
-                        Console.Clear();      
-
-
-                        break;
-
-                    case "D":
-                        Console.WriteLine("Which entry to delete: ");
-                        string del_date = Console.ReadLine();
-                        tablecmd.CommandText = $"DELETE FROM water_habit WHERE date = '{del_date}'";
-                        tablecmd.ExecuteNonQuery();
-                        menu_show = true;
-                        Console.Clear();    
-                        break;
-
-                    case "Q":
-                        App_on = false;
-                        break;
-                    default:
-                        break;
+                    switch (userInput.Trim().ToUpper())
+                    {
+                        case "V":
+                            ViewLogs(tableCmd);
+                            menuShow = true;
+                            Console.Clear();
+                            break;
+                        case "I":
+                            InsertEntry(tableCmd);
+                            menuShow = true;
+                            Console.Clear();
+                            break;
+                        case "U":
+                            UpdateEntry(tableCmd);
+                            menuShow = true;
+                            Console.Clear();
+                            break;
+                        case "D":
+                            DeleteEntry(tableCmd);
+                            menuShow = true;
+                            Console.Clear();
+                            break;
+                        case "Q":
+                            appOn = false;
+                            break;
+                        default:
+                            break;
+                    }
                 }
 
+                connection.Close();
+            }
+        }
+
+        private static void InitializeDatabase()
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+                var tableCmd = connection.CreateCommand();
+
+                tableCmd.CommandText = @"CREATE TABLE IF NOT EXISTS water_habit(
+                                            id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                            Date TEXT,
+                                            Glasses INTEGER)";
+
+                tableCmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+        }
+
+        private static void ShowMenu()
+        {
+            Console.WriteLine("What would you like to do?\n");
+            Console.WriteLine("V - View logs");
+            Console.WriteLine("I - Insert an entry");
+            Console.WriteLine("U - Update an entry");
+            Console.WriteLine("D - Delete an entry");
+            Console.WriteLine("Q - Quit the app");
+        }
+
+        private static bool IsValidOperation(string operation)
+        {
+            return new string[] { "V", "I", "U", "D", "Q" }.Contains(operation.Trim().ToUpper());
+        }
+
+        private static void ViewLogs(SQLiteCommand tableCmd)
+        {
+            tableCmd.CommandText = "SELECT Date, Glasses FROM water_habit";
+            using (SQLiteDataReader reader = tableCmd.ExecuteReader())
+            {
+                Console.WriteLine("Date\t\tGlasses");
+                Console.WriteLine("-------------------------");
+
+                while (reader.Read())
+                {
+                    string date = reader["Date"].ToString();
+                    string glasses = reader["Glasses"].ToString();
+
+                    Console.WriteLine($"{date.PadRight(12)}{glasses}");
+                }
+
+                reader.Close();
             }
 
-            // Close the connection when done
-            connection.Close();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
 
+        private static void InsertEntry(SQLiteCommand tableCmd)
+        {
+            Console.WriteLine("Enter data to be inserted: ");
+            var resultInsert = GetVals();
+
+            tableCmd.CommandText = "INSERT INTO water_habit(Date, Glasses) VALUES(@Date, @Glasses)";
+            tableCmd.Parameters.AddWithValue("@Date", resultInsert.Item1);
+            tableCmd.Parameters.AddWithValue("@Glasses", resultInsert.Item2);
+            tableCmd.ExecuteNonQuery();
+        }
+
+        private static void UpdateEntry(SQLiteCommand tableCmd)
+        {
+            Console.WriteLine("Enter data to be updated");
+            var resultUpdate = GetVals();
+
+            tableCmd.CommandText = "UPDATE water_habit SET Glasses = @Glasses WHERE Date = @Date";
+            tableCmd.Parameters.AddWithValue("@Glasses", resultUpdate.Item2);
+            tableCmd.Parameters.AddWithValue("@Date", resultUpdate.Item1);
+            tableCmd.ExecuteNonQuery();
+        }
+
+        private static void DeleteEntry(SQLiteCommand tableCmd)
+        {
+            Console.WriteLine("Which entry to delete: ");
+            string delDate = Console.ReadLine();
+
+            tableCmd.CommandText = "DELETE FROM water_habit WHERE Date = @Date";
+            tableCmd.Parameters.AddWithValue("@Date", delDate);
+            tableCmd.ExecuteNonQuery();
+        }
+
+        static public (string, int) GetVals()
+        {
+            string inputDate;
+            int inputGlasses;
+            Console.WriteLine("Enter the date (dd-mm-yy): ");
+            inputDate = Console.ReadLine();
+            Console.WriteLine("Enter the amount of glasses: ");
+            inputGlasses = Convert.ToInt32(Console.ReadLine());
+
+            return (inputDate, inputGlasses);
         }
     }
 }
